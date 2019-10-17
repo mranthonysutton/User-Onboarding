0887c8b9557c783d8c58e94897a3f9ccca5589f5
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-17 13:09:51 -0700

    Changed input types to Formik Field

--- user-onboarding/src/Components/FormComponent.js
+++ user-onboarding/src/Components/FormComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { withFormik, Form } from "formik";
+import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import "./FormComponent.css";
 
@@ -9,22 +9,22 @@ const FormComponent = ({ values, touched, errors }) => {
     <div className="container">
       <h1>Add A User</h1>
       <Form className="form-control">
-        <input type="text" name="name" placeholder="Full Name" />
+        <Field type="text" name="name" placeholder="Full Name" />
         {touched.name && errors.name && <p className="error">{errors.name}</p>}
 
-        <input type="email" name="email" placeholder="user@email.com" />
+        <Field type="email" name="email" placeholder="user@email.com" />
         {touched.email && errors.email && (
           <p className="error">{errors.email}</p>
         )}
 
-        <input type="password" name="password" placeholder="Enter Password" />
+        <Field type="password" name="password" placeholder="Enter Password" />
         {touched.password && errors.password && (
           <p className="error">{errors.password}</p>
         )}
 
         <label>
-          <input type="checkbox" name="terms" checked={values.terms}></input>I
-          agree to the terms and conditions
+          <Field type="checkbox" name="terms" />I agree to the terms and
+          conditions
         </label>
 
         {touched.terms && errors.terms && (
@@ -46,11 +46,14 @@ const FormikFormComponent = withFormik({
     };
   },
   validationSchema: Yup.object().shape({
-    name: Yup.string().required("Please enter user's full name"),
+    name: Yup.string().required("Please enter user's name"),
     password: Yup.string().required("Please enter user's password"),
     email: Yup.string().required("Please enter user's email"),
     terms: Yup.boolean().oneOf([true])
-  })
+  }),
+  handleSubmit() {
+    console.log("Submitted");
+  }
 })(FormComponent);
 
 export default FormikFormComponent;
